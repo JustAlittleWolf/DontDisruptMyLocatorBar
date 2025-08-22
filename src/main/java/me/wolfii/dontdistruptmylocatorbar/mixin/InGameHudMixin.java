15c78d385ad662df8957d36d576d151c6ab38527
JustAlittleWolf <jakobi5000@gmail.com>
2025-08-22 09:56:39 +0200

    Initial Commit

---
+++ src/main/java/me/wolfii/dontdistruptmylocatorbar/mixin/InGameHudMixin.java
@@ -0,0 +1,15 @@
+package me.wolfii.dontdistruptmylocatorbar.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+	@WrapOperation(method = "getCurrentBarType", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;shouldShowExperienceBar()Z"))
+	private boolean dontDistruptMyLocatorBar(InGameHud instance, Operation<Boolean> original) {
+		return false;
+	}
+}
