c237db2e7065bd3d2f45a7741070c17f328d2929
Wolfi <wolfi@wolfislaptop>
2026-03-28 10:26:21 +0100

    feat: migrate to mojmap

--- src/main/java/me/wolfii/dontdisruptmylocatorbar/mixin/InGameHudMixin.java
+++ src/main/java/me/wolfii/dontdisruptmylocatorbar/mixin/InGameHudMixin.java
@@ -2,14 +2,14 @@ package me.wolfii.dontdisruptmylocatorbar.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.gui.Gui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(InGameHud.class)
+@Mixin(Gui.class)
 public class InGameHudMixin {
-	@WrapOperation(method = "getCurrentBarType", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;shouldShowExperienceBar()Z"))
-	private boolean dontDisruptMyLocatorBar(InGameHud instance, Operation<Boolean> original) {
+	@WrapOperation(method = "nextContextualInfoState", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;willPrioritizeExperienceInfo()Z"))
+	private boolean dontDisruptMyLocatorBar(Gui instance, Operation<Boolean> original) {
 		return false;
 	}
 }
