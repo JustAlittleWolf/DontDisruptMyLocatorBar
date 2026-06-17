38ece17c98c694a4099ef4b2e34accde6543f12b
JustAlittleWolf <jakobi5000@gmail.com>
2026-06-17 15:03:42 +0200

    feat: Update to 26.2

--- src/main/java/me/wolfii/dontdisruptmylocatorbar/mixin/InGameHudMixin.java
+++ src/main/java/me/wolfii/dontdisruptmylocatorbar/mixin/InGameHudMixin.java
@@ -2,14 +2,14 @@ package me.wolfii.dontdisruptmylocatorbar.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.Hud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(Gui.class)
+@Mixin(Hud.class)
 public class InGameHudMixin {
-	@WrapOperation(method = "nextContextualInfoState", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;willPrioritizeExperienceInfo()Z"))
-	private boolean dontDisruptMyLocatorBar(Gui instance, Operation<Boolean> original) {
-		return false;
-	}
+    @WrapOperation(method = "nextContextualInfoState", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Hud;willPrioritizeExperienceInfo()Z"))
+    private boolean dontDisruptMyLocatorBar(Hud instance, Operation<Boolean> original) {
+        return false;
+    }
 }
